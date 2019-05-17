8e804755f4c0119d4be815144172511f79942569
ischeinkman <scheinkman.ilan@gmail.com>
2019-05-17 01:28:24 -0700

    Removed `shortname` alloc dependency; fixed shortname checksum calculation.

--- src/shortname.rs
+++ src/shortname.rs
@@ -1,13 +1,10 @@
-extern crate alloc;
-use alloc::string::String;
 use core::fmt;
 use core::str::from_utf8_unchecked;
 use core::cmp;
 
 #[derive(Copy, Clone, Debug)]
 pub struct ShortName {
-    pub name: [u8; 8],
-    pub ext: [u8; 3],
+    pub data : [u8 ; 11],
     pub lower_name: bool,
     pub lower_ext: bool,
 }
@@ -15,8 +12,7 @@ pub struct ShortName {
 impl Default for ShortName {
     fn default() -> Self {
         ShortName {
-            name: [b' '; 8],
-            ext: [b' '; 3],
+            data : [b' '; 11],
             lower_name: false,
             lower_ext: false,
         }
@@ -25,7 +21,7 @@ impl Default for ShortName {
 
 impl PartialEq<ShortName> for ShortName {
     fn eq(&self, other: &Self) -> bool {
-        self.name == other.name && self.ext == other.ext
+        self.name() == other.name() && self.ext() == other.ext() 
     }
 }
 
@@ -44,27 +40,9 @@ impl Ord for ShortName {
 
 impl fmt::Display for ShortName {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let name_len = self
-            .name
-            .iter()
-            .rposition(|b| *b != b' ' && *b != 0)
-            .unwrap_or(0);
-        let name_part = if name_len > 0 {
-            unsafe { from_utf8_unchecked(&self.name[..name_len]) }
-        } else {
-            "{NONE}"
-        };
-        let ext_len = self
-            .ext
-            .iter()
-            .rposition(|b| *b != b' ' && *b != 0)
-            .unwrap_or(0);
-        let ext_part = if ext_len > 0 {
-            unsafe { from_utf8_unchecked(&self.ext[..ext_len]) }
-        } else {
-            "{NONE}"
-        };
-        write!(f, "ShortName{{ name: {}, ext : {} }}", name_part, ext_part)
+        let name = if self.lower_name { self.name().to_ascii_lowercase() } else { self.name().to_ascii_uppercase() };
+        let ext = if self.lower_ext { self.ext().to_ascii_lowercase() } else { self.ext().to_ascii_uppercase() };
+        write!(f, "ShortName{{ name: {}, ext : {} }}", name, ext)
     }
 }
 
@@ -74,20 +52,35 @@ impl ShortName {
     pub const SHORT_NAME_FULL_LENGTH: usize = Self::SHORT_NAME_EXT_LENGTH + Self::SHORT_NAME_LENGTH;
 
     pub fn read_byte(self, idx: usize) -> u8 {
-        match idx {
-            0 => {
-                if self.name[0] == 0xE5 {
-                    0x05
-                } else {
-                    self.name[0]
-                }
-            }
-            b @ 1..=7 => self.name[b],
-            b @ 7..=10 => self.ext[b - 8],
-            _ => 0,
+        if idx == 0 && self.data[0] == 0xE5 {
+            0x05
+        }
+        else if idx < self.data.len() {
+            self.data[idx]
+        }
+        else {
+            0
         }
     }
 
+    pub fn name_len(self) -> usize {
+        (&self.data[..8]).iter().take_while(|&&c| !is_end_marker(c.into())).count()
+    }
+
+    pub fn ext_len(self) -> usize {
+        (&self.data[8..]).iter().take_while(|&&c| !is_end_marker(c.into())).count()
+    }
+
+    pub fn name(&self) -> &str {
+        unsafe {
+            from_utf8_unchecked(&self.data[..self.name_len()])
+        }
+    }
+    pub fn ext(&self) -> &str {
+        unsafe {
+            from_utf8_unchecked(&self.data[8..8 + self.ext_len()])
+        }
+    }
     pub fn case_flag(self) -> u8 {
         match (self.lower_name, self.lower_ext) {
             (true, true) => 0x18,
@@ -97,33 +90,10 @@ impl ShortName {
         }
     }
 
-    pub fn to_string(self) -> String {
-        let mut retval = String::new();
-        for name_idx in 0..8 {
-            if self.name[name_idx] == b' ' || self.name[name_idx] == 0 {
-                break;
-            }
-            let namechar: char = self.name[name_idx].into();
-            if self.lower_name {
-                retval.push(namechar.to_ascii_lowercase());
-            } else {
-                retval.push(namechar.to_ascii_uppercase());
-            }
+    pub fn to_str(&self) -> &str {
+        unsafe {
+            from_utf8_unchecked(&self.data)
         }
-        retval.push('.');
-        for ext_idx in 0..3 {
-            if self.ext[ext_idx] == b' ' || self.ext[ext_idx] == 0 {
-                break;
-            }
-            let extchar: char = self.ext[ext_idx].into();
-            if self.lower_ext {
-                retval.push(extchar.to_ascii_lowercase());
-            } else {
-                retval.push(extchar.to_ascii_uppercase());
-            }
-        }
-
-        retval
     }
 
     pub fn from_str<T: AsRef<str>>(name: T) -> Option<ShortName> {
@@ -148,7 +118,7 @@ impl ShortName {
                 retval.lower_name = case == 1;
             }
 
-            retval.name[idx] = char_to_byte(c);
+            retval.data[idx] = char_to_byte(c);
         }
         if ext_idx == 0 {
             return None;
@@ -168,12 +138,14 @@ impl ShortName {
                 retval.lower_ext = case == 1;
             }
 
-            retval.ext[idx] = char_to_byte(c);
+            retval.data[idx + 8] = char_to_byte(c);
         }
         Some(retval)
     }
 
     pub fn convert_str<T: AsRef<str>>(name: T, duplicate_count: u8) -> ShortName {
+        let mut retval = ShortName::default();
+        
         let name: &str = name.as_ref();
         if let Some(r) = ShortName::from_str(name) {
             return r;
@@ -183,39 +155,39 @@ impl ShortName {
             .rfind(|(_, c)| *c == '.')
             .map(|(idx, _)| idx);
         let (name_part_raw, ext_part_raw) = ext_idx.map_or((name, ""), |idx| name.split_at(idx));
-        let mut name_part = to_valid_shortname(name_part_raw);
-        while name_part.len() < 8 {
-            name_part.push(' ');
+        let name_part = to_valid_shortname(name_part_raw);
+        let mut name_part_idx = 0;
+        for c in name_part {
+            retval.data[name_part_idx] = char_to_byte(c);
+            name_part_idx += 1;
         }
-        let mut ext_part = to_valid_shortname(ext_part_raw);
-        while ext_part.len() < 3 {
-            ext_part.push(' ');
+        let ext_part = to_valid_shortname(ext_part_raw);
+        let mut ext_part_idx = 0;
+        for c in ext_part {
+            retval.data[ext_part_idx + 8] = char_to_byte(c);
+            ext_part_idx += 1;
         }
-
-        let mut retval = ShortName::default();
-        (&mut retval.ext).copy_from_slice(&ext_part.as_bytes()[0..3]);
-        (&mut retval.name).copy_from_slice(&name_part.as_bytes()[0..8]);
         if duplicate_count == 0 {
-            retval.name[6] = b'~';
-            retval.name[7] = b'~';
+            retval.data[6] = b'~';
+            retval.data[7] = b'~';
         } else {
             let mut suffix_digits_left = duplicate_count;
             let mut cur_idx = 7;
             while suffix_digits_left > 0 {
                 let digit = suffix_digits_left % 10;
                 let digit_char = digit + b'0';
-                retval.name[cur_idx] = digit_char;
+                retval.data[cur_idx] = digit_char;
                 cur_idx -= 1;
                 suffix_digits_left /= 10;
             }
-            retval.name[cur_idx] = b'~';
+            retval.data[cur_idx] = b'~';
         }
         retval
     }
 
     pub fn lfn_checksum(&self) -> u8 {
         let mut retval: u8 = 0;
-        for c in self.name.iter() {
+        for c in self.data.iter() {
             let shifted_retval = ((retval & 1) << 7) + ((retval & 0xFE) >> 1);
             retval = shifted_retval.wrapping_add(*c);
         }
@@ -229,6 +201,7 @@ fn char_to_byte(assumed_valid: char) -> u8 {
     assumed_valid.encode_utf8(&mut tmpbuff);
     tmpbuff[0]
 }
+
 fn is_valid_char(inp: char) -> bool {
     inp.len_utf8() == 1
         && (inp.is_ascii_uppercase()
@@ -262,8 +235,7 @@ fn case_val(inp: char) -> u8 {
     }
 }
 
-fn to_valid_shortname<T: AsRef<str>>(raw: T) -> String {
-    let raw: &str = raw.as_ref();
+fn to_valid_shortname<'a>(raw: &'a str) -> impl Iterator<Item=char> +'a {
     raw.chars()
         .filter_map(|c| {
             if is_end_marker(c) {
@@ -274,16 +246,4 @@ fn to_valid_shortname<T: AsRef<str>>(raw: T) -> String {
                 Some(c.to_ascii_uppercase())
             }
         })
-        .collect()
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn test_basic_contersion() {
-        let test_str = "testflb.txt";
-        let gotten = ShortName::from_str(&test_str).unwrap();
-        assert_eq!(test_str, &gotten.to_string());
-    }
 }
