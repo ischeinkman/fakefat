f5e8160a7dafeae80c5dd8fa50eb567f504d2144
ischeinkman <scheinkman.ilan@gmail.com>
2019-05-17 01:25:18 -0700

    Removed alloc dependency from `longname`.

--- src/longname.rs
+++ src/longname.rs
@@ -1,6 +1,5 @@
 use crate::dirent::{FileDirEntry, LfnDirEntry};
 use crate::shortname::ShortName;
-use alloc::vec::Vec;
 
 pub fn lfn_count_for_name(name: &str) -> usize {
     if ShortName::from_str(name).is_some() {
@@ -9,24 +8,24 @@ pub fn lfn_count_for_name(name: &str) -> usize {
     name.len() / 13 + if name.len() % 13 != 0 { 1 } else { 0 }
 }
 
-pub fn construct_name_entries(name: &str, base: FileDirEntry) -> Vec<LfnDirEntry> {
-    let mut retval = Vec::new();
-    if name == base.name.to_string() {
-        return retval;
+pub fn construct_name_entries<EntryType : From<LfnDirEntry>, BuffType : AsMut<[EntryType]>> (name: &str, base: FileDirEntry, mut allocation : BuffType) {
+    let entries_len = lfn_count_for_name(name);
+    if entries_len == 0 {
+        return;
     }
-
+    let buff = allocation.as_mut();
     let checksum = base.name.lfn_checksum();
-    let entries_len = name.len() / 13 + if name.len() % 13 != 0 { 1 } else { 0 };
-    retval.reserve(entries_len);
+    let entries_len = lfn_count_for_name(name);
+    debug_assert!(entries_len > 0, "Got count-entry mismatch: {} for {}.", entries_len, name);
+    debug_assert!(entries_len <= buff.len(), "Bad allocation: needed {} entries but only got buffer length {}.", entries_len, buff.len());
+
     for (idx, part) in name.as_bytes().chunks(13).enumerate() {
         let mut newent = LfnDirEntry::default();
-        newent.entry_num = idx as u8;
+        newent.entry_num = if idx == 0 { 0x40 | (entries_len as u8) } else { entries_len as u8 - idx as u8} ;
         newent.checksum = checksum;
 
         let part_len = part.len();
         (&mut newent.name_part[..part_len]).copy_from_slice(part);
-        retval.push(newent);
+        buff[idx] = newent.into();
     }
-    retval[0].entry_num |= 0x40;
-    retval
 }
